98e220d43094ac46643a78a3a622d1236f453744
MrUnderSan <MrUnderSan@gmail.com>
2023-08-16 14:23:41 +0300

    completed 01 - 01

---
+++ src/components/accordion/Accordion.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+type AccordionPropsType = {
+    title: string,
+    collapsed: boolean
+}
+
+export function Accordion(props: AccordionPropsType) {
+    if (props.collapsed) {
+        return (
+            <>
+                <AccordionTitle title={props.title}/>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <AccordionTitle title={props.title}/>
+                <AccordionBody/>
+            </>
+        )
+    }
+}
+
+type AccordionTitlePropsType = {
+    title: string
+}
+function AccordionTitle(props: AccordionTitlePropsType) {
+    return (
+        <h3>{props.title}</h3>
+    )
+}
+
+function AccordionBody() {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
